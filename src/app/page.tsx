d45bb7bece72a1a3e85ab738cc71d8ebf3cf5473
Sunny Jha <jhasunny550@gmail.com>
2025-03-25 16:55:55 +0530

    add second add to cart button with GTM event tracking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,16 @@ export default function Home() {
     });
   };
 
+  const handleAddToCart2 = () => {
+    sendGTMEvent({
+      event: "add_to_cart",
+      product_id: "P002",
+      product_name: "Laptop",
+      product_price: 59999,
+      currency: "INR",
+      quantity:5})
+  }
+
   
 
   return (
@@ -22,6 +32,9 @@ export default function Home() {
         onClick={handleAddToCart}
       >
         Add to Cart
+      </button>
+      <button className="bg-red-500 hover:bg-reed-700 text-white font-bold py-2 px-4 rounded" onClick={handleAddToCart2}>
+        Add to cart2
       </button>
       <h1 className="text-rose-500 font-bold">Hello </h1>
     </div>
